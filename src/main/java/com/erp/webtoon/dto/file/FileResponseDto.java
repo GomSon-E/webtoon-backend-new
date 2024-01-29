4fd9a7d8b059c2091cbfe1edc462ab1fb04df5b0
Gomson-E <gomsonixx@gmail.com>
2024-01-29 13:09:28 +0900

    DOCS : 파일 DTO - Swagger API 문서화

--- src/main/java/com/erp/webtoon/dto/file/FileResponseDto.java
+++ src/main/java/com/erp/webtoon/dto/file/FileResponseDto.java
@@ -1,15 +1,21 @@
 package com.erp.webtoon.dto.file;
 
 import com.erp.webtoon.domain.File;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
+@ApiModel(description = "파일 조회 응답 DTO")
 public class FileResponseDto {
 
+    @ApiModelProperty(value = "파일 ID", example = "1")
     private Long fileId;
 
+    @ApiModelProperty(value = "원본파일명", example = "example.png")
     private String fileOriginName;
 
+    @ApiModelProperty(value = "파일 경로", example = "http://example:8080/example.png")
     private String filePath;
 
     public FileResponseDto(File file) {
